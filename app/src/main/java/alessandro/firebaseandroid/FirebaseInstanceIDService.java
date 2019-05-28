edbe8e8b2162dd6eac3d4247573bc4dcb375cbb9
knut-sdpark <sdp1422@naver.com>
2019-05-28 09:47:19 +0900

    modified 'build_gradle version to 3.0.1 & google-services version to 4.0.1' & created 'FirebaseInstanceIDService, FireBaseMessagingService java class'

---
+++ app/src/main/java/alessandro/firebaseandroid/FirebaseInstanceIDService.java
@@ -0,0 +1,29 @@
+package alessandro.firebaseandroid;
+
+import android.annotation.SuppressLint;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseInstanceIDService";
+
+    @SuppressLint("LongLogTag")
+    @Override
+    public void onTokenRefresh() {
+
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        sendRegistrationToServer(refreshedToken);
+
+    }
+
+    private void sendRegistrationToServer(String token) {
+
+    }
+
+}
